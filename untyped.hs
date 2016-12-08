ca77abea242aee457c6ca04f07835ca991254386
Neil Parikh <neil@tophat.com>
2016-12-08 11:55:58 -0500

    Implement untyped lambda calculus

---
+++ untyped.hs
@@ -0,0 +1,33 @@
+main :: IO ()
+main = print $ eval exampleExpr
+
+--((λx.((λy.(x y))x))(λz.w))
+exampleExpr :: Expr
+exampleExpr = App (App (Lambda 'x' (Lambda 'y' (Variable 'y')))
+                       (Variable 'a'))
+                  (Variable 'b')
+
+type Name = Char
+
+data Expr = Variable Name
+          | Lambda Name Expr
+          | App Expr Expr
+          deriving Show
+
+eval :: Expr -> Expr
+eval (Variable x) = Variable x
+eval (Lambda x body) = Lambda x body
+eval (App e1 e2) = case (eval e1) of
+    Lambda x body -> eval (substitute body x e2)
+    result -> App result e2
+
+substitute :: Expr -> Name -> Expr -> Expr
+substitute (Variable a) b expr
+    | a == b = expr
+    | otherwise = (Variable a)
+substitute (Lambda a body) b expr
+    | a == b = (Lambda a body)
+    | otherwise = Lambda a (substitute body b expr)
+substitute (App e1 e2) b expr = App (subHelper e1) (subHelper e2)
+    where
+    subHelper baseExpr = substitute baseExpr b expr
