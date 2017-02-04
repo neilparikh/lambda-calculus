87e28ae1a984c122d726f4b407c62166d6b12bb4
Neil Parikh <parikh.neil@me.com>
2017-02-04 16:50:07 -0500

    add type type and annotate constructor

--- types.hs
+++ types.hs
@@ -5,5 +5,9 @@ type Name = Char
 data Expr = Variable Name
           | Lambda Name Expr
           | App Expr Expr
+          | Annotate Expr Type
           deriving Show
 
+data Type = Base Name
+          | Function Type Type
+          deriving Show
