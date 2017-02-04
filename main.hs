9e20cf814dd7dea6ff0dfbea31489855bd25a657
Neil Parikh <parikh.neil@me.com>
2017-02-04 16:50:21 -0500

    don't shadow error

--- main.hs
+++ main.hs
@@ -5,5 +5,5 @@ main :: IO ()
 main = do
     putStrLn $ case applyParser exprParser "(((|x.(|y.(xy)))(|a.a))r)" of
         Right expr -> show . eval $ expr
-        Left error -> show error
+        Left err -> show err
 
