fa14e56876993d9643000114e85904c3d6b00f04
Neil Parikh <parikh.neil@me.com>
2017-02-04 22:47:00 -0500

    little bit of cleanup

--- main.hs
+++ main.hs
@@ -4,7 +4,8 @@ import Types (Expr(Annotate))
 
 main :: IO ()
 main = do
-    putStrLn $ case applyParser exprParser "((|x.(|y.(|z.(y(xz))))) : ((A -> B) -> ((B -> C) -> (A -> C))))" of
+    let lambdaExpr = "((|x.(|y.(|z.(y(xz))))) : ((A -> B) -> ((B -> C) -> (A -> C))))"
+    putStrLn $ case applyParser exprParser lambdaExpr of
         Right (Annotate expr t) -> show expr ++ " : " ++ show t ++ " = " ++ show (typeCheck emptyCtx expr t)
         Right _ -> show "to be implemented"
         Left err -> show err
