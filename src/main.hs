722ce35971a35b4398c436ae8b38d2da31440f51
Neil Parikh <parikh.neil@me.com>
2017-12-10 21:08:04 -0500

    some work to add other type systems

--- src/main.hs
+++ src/main.hs
@@ -1,13 +1,19 @@
-import SimplyTyped.Parser (applyParser, exprParser, typeParser)
-import SimplyTyped.TypeChecker (typeCheck, emptyCtx)
+import qualified SimplyTyped.Parser as S (applyParser, exprParser, typeParser)
+import qualified SimplyTyped.TypeChecker as S (typeCheck, emptyCtx)
 
 main :: IO ()
 main = do
+    putStrLn "Which type system would you like to run?"
+    putStrLn "- SimplyTyped"
+    input <- getLine
+    let (exprParse, typeParse, typeCheck, emptyCtx) = case input of
+            "SimplyTyped" -> (S.applyParser S.exprParser, S.applyParser S.typeParser, S.typeCheck, S.emptyCtx)
+            _ -> error "invalid option"
     let lambdaExpr = "(|x.(|y.(|z.(y(xz)))))"
     let lambdaType = "((A -> B) -> ((B -> C) -> (A -> C)))"
     let exprAndType = do
-            expr <- applyParser exprParser lambdaExpr
-            t <- applyParser typeParser lambdaType
+            expr <- exprParse lambdaExpr
+            t <- typeParse lambdaType
             pure (expr, t)
     putStrLn $ case exprAndType of
         Right (expr, t) -> show expr ++ " : " ++ show t ++ " = " ++ show (typeCheck emptyCtx expr t)
