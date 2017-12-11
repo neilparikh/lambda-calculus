f3a2f0a0a5161695dceb332f0cd6d2593b370fd5
Neil Parikh <parikh.neil@me.com>
2017-12-10 21:18:20 -0500

    add eval to main

--- src/main.hs
+++ src/main.hs
@@ -1,13 +1,14 @@
 import qualified SimplyTyped.Parser as S (applyParser, exprParser, typeParser)
 import qualified SimplyTyped.TypeChecker as S (typeCheck, emptyCtx)
+import qualified SimplyTyped.Eval as S (eval)
 
 main :: IO ()
 main = do
     putStrLn "Which type system would you like to run?"
     putStrLn "- SimplyTyped"
     input <- getLine
-    let (exprParse, typeParse, typeCheck, emptyCtx) = case input of
-            "SimplyTyped" -> (S.applyParser S.exprParser, S.applyParser S.typeParser, S.typeCheck, S.emptyCtx)
+    let (exprParse, typeParse, typeCheck, emptyCtx, eval) = case input of
+            "SimplyTyped" -> (S.applyParser S.exprParser, S.applyParser S.typeParser, S.typeCheck, S.emptyCtx, S.eval)
             _ -> error "invalid option"
     let lambdaExpr = "(|x.(|y.(|z.(y(xz)))))"
     let lambdaType = "((A -> B) -> ((B -> C) -> (A -> C)))"
@@ -15,7 +16,9 @@ main = do
             expr <- exprParse lambdaExpr
             t <- typeParse lambdaType
             pure (expr, t)
-    putStrLn $ case exprAndType of
-        Right (expr, t) -> show expr ++ " : " ++ show t ++ " = " ++ show (typeCheck emptyCtx expr t)
-        Left err -> show err
+    case exprAndType of
+        Right (expr, t) -> do
+            putStrLn $ show expr ++ " : " ++ show t ++ " = " ++ show (typeCheck emptyCtx expr t)
+            putStrLn . show $ eval expr
+        Left err -> putStrLn $ show err
 
